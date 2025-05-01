b3573d849a20e0d49c1dc9c98ff256514180997a
Ishan Karki <legendishan007@gmail.com>
2025-05-02 00:08:08 +0545

    Add Expense Progress

--- lib/screens/add_expense/views/add_expense.dart
+++ lib/screens/add_expense/views/add_expense.dart
@@ -7,21 +7,28 @@ class AddExpense extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.surface,
-      ),
+      appBar: AppBar(backgroundColor: Theme.of(context).colorScheme.surface),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Add Expense',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.w700,
-            ),
+            Text(
+              'Add Expense',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
             ),
             TextFormField(),
+            SizedBox(height: 16),
+            TextFormField(),
+            SizedBox(height: 16),
+            TextFormField(),
+            SizedBox(height: 16),
+            TextButton(
+              onPressed: () {
+                print('Saved pressed');
+              },
+              child: Text('Save'),
+            ),
           ],
         ),
       ),
